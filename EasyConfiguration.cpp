440907dc2cf56c69f0c5e6fc00fd63ee990e7787
=xemuth <=hamonclem@hotmail.fr>
2019-06-01 01:40:43 +0200

    correction bug

--- EasyConfiguration.cpp
+++ EasyConfiguration.cpp
@@ -7,23 +7,17 @@ CONSOLE_APP_MAIN
 	EasyConfiguration ez;
 	try{
 		ez.SetValue<bool>("test",true); 
-		ez.SetValue<String>("test2", "Hello world !");
+		ez.SetValue<String>("test2", "Hello World");
 		ez.SetValue<int>("test3",123);
 		ez.SetValue<double>("test4",123.0);
 	}catch(Upp::ValueTypeError &e){
 		Cout() <<"Exception"<< e <<"\n";	
 	}
-	bool test1 = ez.GetValue<bool>("test");
-	String test2 = ez.GetValue<String>("test2");
-	int test3 = ez.GetValue<int>("test3");
-	double test4 = ez.GetValue<double>("test4");
-	int test5 = ez.GetValue<int>("test5");
-	
-	Cout() << "test1 : " <<test1 << "\n";
-	Cout() << "test2 : " <<test2 << "\n";
-	Cout() << "test3 : " <<test3 << "\n";
-	Cout() << "test4 : " <<test4 << "\n";
-	Cout() << "Test5 (Int non definie) : " << test5 <<"\n";
+	Cout() << "test1 : " << ez.GetValue<bool>("test") << "\n";
+	Cout() << "test2 : " << ez.GetValue<String>("test2") << "\n";
+	Cout() << "test3 : " << ez.GetValue<int>("test3") << "\n";
+	Cout() << "test4 : " << ez.GetValue<double>("test4") << "\n";
+	Cout() << "Test5 (Int non definie) : " << ez.GetValue<int>("test5") <<"\n";
 }
 
 template <class T>   
@@ -35,11 +29,10 @@ T EasyConfiguration::GetValue(String fieldName){
 	return T();
 }
 
-template <class T> 
+template <class T>
 bool EasyConfiguration::SetValue(String fieldName, const T &t){
 	try{
-		Value v(t);
-		if(ConfigurationType.Add(fieldName,v))
+		if(&ConfigurationType.Add(fieldName,Value(t)))
 			return true;
 	}catch(Upp::ValueTypeError &e){
 		throw e;	
@@ -47,7 +40,7 @@ bool EasyConfiguration::SetValue(String fieldName, const T &t){
 }
 
 
-
 EasyConfiguration::EasyConfiguration(){
 }
+
 
--- EasyConfiguration.h
+++ EasyConfiguration.h
@@ -3,16 +3,17 @@
 #include <Core/Core.h>
 
 using namespace Upp;
-
 class EasyConfiguration{
 	private:
-		VectorMap<String,Value> ConfigurationType;
+		VectorMap<String,Upp::Value> ConfigurationType;
+		String pathToLocalConfig="";
+		String pathToGlobalConfig=""
 	public:
 		EasyConfiguration();
 		
 		template <class T> T GetValue(String fieldName);
-		template <class T> bool SetValue(String fieldName,const T &t);
+		template <class T> bool SetValue(String fieldName, const T &t);
+		
+		friend void PrepareDefaultConfiguration();
 };
-
-
 #endif
